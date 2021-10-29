0f2d174f59279fbb09dd95946c296e667a272f51
Felix Lebel <felix@vynatu.io>
2021-10-29 11:35:37 -0400

    first commit

---
+++ MemoryTrackerForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace MaplestoryMemoryTracker
+{
+    partial class MemoryTrackerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MemoryStatusLabel = new System.Windows.Forms.Label();
+            this.ForceForegroundCheckbox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // MemoryStatusLabel
+            // 
+            this.MemoryStatusLabel.AutoSize = true;
+            this.MemoryStatusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MemoryStatusLabel.Location = new System.Drawing.Point(5, 8);
+            this.MemoryStatusLabel.Name = "MemoryStatusLabel";
+            this.MemoryStatusLabel.Size = new System.Drawing.Size(201, 25);
+            this.MemoryStatusLabel.TabIndex = 1;
+            this.MemoryStatusLabel.Text = "Game not running...";
+            // 
+            // ForceForegroundCheckbox
+            // 
+            this.ForceForegroundCheckbox.AutoSize = true;
+            this.ForceForegroundCheckbox.Location = new System.Drawing.Point(218, 32);
+            this.ForceForegroundCheckbox.Name = "ForceForegroundCheckbox";
+            this.ForceForegroundCheckbox.Size = new System.Drawing.Size(84, 17);
+            this.ForceForegroundCheckbox.TabIndex = 2;
+            this.ForceForegroundCheckbox.Text = "Stay on Top";
+            this.ForceForegroundCheckbox.UseVisualStyleBackColor = true;
+            this.ForceForegroundCheckbox.CheckStateChanged += new System.EventHandler(this.OnForegroundCheckboxChanged);
+            // 
+            // MemoryTrackerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 53);
+            this.Controls.Add(this.ForceForegroundCheckbox);
+            this.Controls.Add(this.MemoryStatusLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MemoryTrackerForm";
+            this.Text = "Maplestory Memory Tracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label MemoryStatusLabel;
+        private System.Windows.Forms.CheckBox ForceForegroundCheckbox;
+    }
+}
+
---
+++ MemoryTrackerForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaplestoryMemoryTracker
+{
+    public partial class MemoryTrackerForm : Form
+    {
+        private readonly Timer _memoryTrackerTimer = new Timer();
+        private const string ProcessName = "MapleStory";
+
+        public MemoryTrackerForm()
+        {
+            InitializeComponent();
+
+            _memoryTrackerTimer.Interval = 5000; // Every 5 seconds, check status of maple
+            _memoryTrackerTimer.Tick += MemoryTrackerTimerOnTick;
+
+            _memoryTrackerTimer.Start();
+
+            // Trigger a tick immediately;
+            MemoryTrackerTimerOnTick();
+        }
+
+        private void MemoryTrackerTimerOnTick(object sender = null, EventArgs e = null)
+        {
+            var processList = Process.GetProcessesByName(ProcessName);
+
+            if (processList.Length == 0)
+            {
+                MemoryStatusLabel.Text = @"Game not running...";
+                this.ResetBackColor();
+                MemoryStatusLabel.ForeColor = Color.Black;
+
+                return;
+            }
+
+            var process = processList[0];
+            process.Refresh(); // Clear cache
+
+            var sizeInGigabytes = ToGigabyte(process.WorkingSet64);
+
+            // Now check how unstable this is
+            var maxGood = (long) (2 /* Gigabytes */ * Math.Pow(1024, 3));
+            var maxForGrinding = (long) Math.Floor(2.8 * Math.Pow(1024, 3));
+
+            string status;
+
+            if (process.WorkingSet64 <= maxGood)
+            {
+                status = "All Good";
+                BackColor = Color.LimeGreen;
+                MemoryStatusLabel.ForeColor = Color.Black;
+            } else if (process.WorkingSet64 <= maxForGrinding)
+            {
+                status = "Warning";
+                BackColor = Color.Yellow;
+                MemoryStatusLabel.ForeColor = Color.Black;
+            }
+            else
+            {
+                status = "Danger!";
+                BackColor = Color.Red;
+                MemoryStatusLabel.ForeColor = Color.White;
+            }
+
+            MemoryStatusLabel.Text = $@"{status} ({sizeInGigabytes} GB)";
+        }
+
+        private static string ToGigabyte(long value)
+        {
+            return (value / Math.Pow(1024, 3)).ToString("0.000");
+        }
+
+        private void OnForegroundCheckboxChanged(object sender, EventArgs e)
+        {
+            TopMost = ForceForegroundCheckbox.Checked;
+        }
+    }
+}
